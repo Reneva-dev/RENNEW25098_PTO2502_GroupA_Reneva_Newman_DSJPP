1b3cf4c276554e25ae10ddbfd0596acc9eb5f684
Reneva Newman <renevanewman7@gmail.com>
2025-11-16 11:03:38 +0500

    modified app.jsx to include favourites route

--- DJS05-SOLUTION-main/src/App.jsx
+++ DJS05-SOLUTION-main/src/App.jsx
@@ -5,7 +5,9 @@ import ShowDetail from "./pages/ShowDetail";
 
 import { PodcastProvider } from "./context/PodcastContext";
 import { AudioPlayerProvider } from "./context/AudioPlayerContext";
-import { FavouritesProvider } from "./context/FavouritesContext"; // <-- NEW IMPORT
+import { FavouritesProvider } from "./context/FavouritesContext"; // <-- NEW
+
+import FavouritesPage from "./pages/FavouritesPage"; // <-- NEW IMPORT
 
 import AudioPlayer from "./components/UI/AudioPlayer";
 
@@ -17,24 +19,23 @@ export default function App() {
       {/* Wrap entire app inside all providers */}
       <PodcastProvider>
         <AudioPlayerProvider>
-          <FavouritesProvider>  {/* <-- NEW WRAPPER */}
+          <FavouritesProvider>
 
             {/* MAIN WRAPPER - adds bottom padding so the audio player doesn't overlap content */}
             <div style={{ paddingBottom: "90px" }}>
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/show/:id" element={<ShowDetail />} />
+                <Route path="/favourites" element={<FavouritesPage />} /> {/* <-- NEW ROUTE */}
               </Routes>
             </div>
 
             {/* 🌟 Persistent Global Audio Player */}
             <AudioPlayer />
 
-          </FavouritesProvider> {/* END */}
+          </FavouritesProvider>
         </AudioPlayerProvider>
       </PodcastProvider>
     </>
   );
 }
-
-
